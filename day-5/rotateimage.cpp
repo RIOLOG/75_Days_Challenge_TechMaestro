f64ea7badca24adf5b3fa148394fb32e4ddf9d26
Ankit Singh <84015430+RIOLOG@users.noreply.github.com>
2022-03-29 23:50:35 +0530

    Add files via upload

---
+++ day-5/rotateimage.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) 
+    {
+        //transpose of matrix:
+        for (int i=0;i<matrix.size();i++)
+        {
+            for (int j=i+1;j<matrix[0].size();j++)
+            {
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        
+        //reverse of column:
+        for (int i=0;i<matrix.size();i++)
+        {
+            int low=0;
+            int high=matrix.size()-1;
+            
+            while(low<high)
+            {
+                swap(matrix[i][low],matrix[i][high]);
+                low+=1;
+                high-=1;
+            }
+        }
+        
+    }
+};
